aa92f2ea0650ecad7657a111a502f337e4821b61
BinbinvoGamingVNN <122621965+BinbinvoGamingVNClone@users.noreply.github.com>
2025-02-20 21:09:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('logger.php')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("info").innerHTML = `
-                <p><strong>IP:</strong> ${data.ip}</p>
-                <p><strong>Provider:</strong> ${data.provider}</p>
-                <p><strong>Country:</strong> ${data.country}</p>
-                <p><strong>City:</strong> ${data.city}</p>
-                <p><strong>OS:</strong> ${data.os}</p>
-                <p><strong>Browser:</strong> ${data.browser}</p>
-            `;
-        });
+    fetch('https://ipapi.co/json/')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("info").innerHTML = `
+            <p><strong>IP:</strong> ${data.ip}</p>
+            <p><strong>Provider:</strong> ${data.org || "Unknown"}</p>
+            <p><strong>Country:</strong> ${data.country_name}</p>
+            <p><strong>City:</strong> ${data.city}</p>
+            <p><strong>OS:</strong> Unknown</p>
+            <p><strong>Browser:</strong> Unknown</p>
+        `;
+    });
 });
